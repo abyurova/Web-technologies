8f09f9ab62909ab7c4d25fb7db721297b1d61463
abyurova <ainurabyurova@gmail.com>
2017-10-10 19:54:18 +0600

    Initial commit

---
+++ secondLab/index.ts
@@ -0,0 +1,5 @@
+declare var a : number;
+a=5;
+declare var b :number;
+b=3;
+console.log(a+b);
---
+++ secondLab/src/operations/sum.operations.ts
@@ -0,0 +1,9 @@
+import {IOperation} from "../operation";
+export class SumOperation implements IOperation {
+    public param1:number;
+    public param2:number;
+
+    execuute():number{
+        const result = this.param1
+    }
+} 
